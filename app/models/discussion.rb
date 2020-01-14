f14c542dde14f31fa529482edd09506f19213dca
Daniel Trujillo <ganimedes@firemail.cc>
2020-01-14 16:16:32 -0500

    added model validations

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,3 +1,7 @@
 class Discussion < ApplicationRecord
+  # model association
   belongs_to :topic
+  has_many :posts, dependent: :destroy
+  #validations
+  validates :author, :title, :message, presence: true
 end
