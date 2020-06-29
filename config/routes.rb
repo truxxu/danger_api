e4cd537adf36ceab63e66db801fd318bb787104a
Daniel Trujillo <danielfetrujillo@gmail.com>
2020-06-29 07:13:25 -0500

    changed routing to deep nesting!

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   # Follows good practice of shallow nesting
-  resources :topics, only: [:show, :index] do
-    resources :discussions, only: [:index, :new, :create, :show, :destroy]
-  end
-  resources :discussions, only: [] do
-    resources :posts, only: [:index, :new, :create]
-  end
+  # resources :topics, only: [:show, :index] do
+  #   resources :discussions, only: [:index, :new, :create, :show, :destroy]
+  # end
+  # resources :discussions, only: [] do
+  #   resources :posts, only: [:index, :new, :create]
+  # end
 
   # Not recommended
-  # resources :topics, only: [:show, :index] do
-  #   resources :discussions, only: [:index, :new, :create, :show] do
-  #     resources :posts, only: [:index, :new, :create]
-  #   end
-  # end
+  resources :topics, only: [:show, :index] do
+    resources :discussions, only: [:index, :new, :create, :show] do
+      resources :posts, only: [:index, :new, :create]
+    end
+  end
 end
